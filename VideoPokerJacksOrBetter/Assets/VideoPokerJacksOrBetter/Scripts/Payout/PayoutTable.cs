6b82ad04c82fec78a5c849bc29f00952f963da69
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2019-09-26 13:04:53 -0700

    Issue#2 - completed initialization of PayoutTable, changed text values for Hands childs in MainScenes for PayoutTable to lover case to see the difference in values after running

--- VideoPokerJacksOrBetter/Assets/VideoPokerJacksOrBetter/Scripts/Payout/PayoutTable.cs
+++ VideoPokerJacksOrBetter/Assets/VideoPokerJacksOrBetter/Scripts/Payout/PayoutTable.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 public class PayoutTable : MonoBehaviour
 {
     private static string CLASS_NAME = typeof(PayoutTable).ToString();
-    private PayoutTableModel payoutTableModel;
+
+    public const string HAND_COLUMN = "Hands";
+    public const string COIN_1_COLUMN = "Coin_1";
+    public const string COIN_2_COLUMN = "Coin_2";
+    public const string COIN_3_COLUMN = "Coin_3";
+    public const string COIN_4_COLUMN = "Coin_4";
+    public const string COIN_5_COLUMN = "Coin_5";
+
+    private PayoutTableModel payoutTableModel = null;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,104 +23,60 @@ public class PayoutTable : MonoBehaviour
         DebugUtil.Instance.PrintD(CLASS_NAME, "Start");
         payoutTableModel = new PayoutTableModel();
 
-
         Transform[] allTransforms = GetComponentsInChildren<Transform>(true);
 
         foreach (Transform child in allTransforms)
         {
-                if (child.gameObject.name == "Hands")
-                {
-                    Transform[] handTransforms = child.GetComponentsInChildren<Transform>(true);
-
-                    foreach (Transform handChild in handTransforms)
-                    {
-                        string searchLabel = "txtMP_Element_";
-                        string goName = handChild.gameObject.name;
-
-                        if(goName.Contains(searchLabel))
-                        {
-                            string handNumberString = Regex.Match(goName, @"\d+").Value;
-                            int handNumber = int.Parse(handNumberString);
-
-                            TMPro.TextMeshProUGUI txt = handChild.gameObject.GetComponent<TMPro.TextMeshProUGUI>();
-
-                           txt.text=  payoutTableModel.payouts[handNumber].handName;
-
-
-
-                        }
-                    }
-                }
-
-                if (child.gameObject.name == "Coin_1")
-                {
-                    Transform[] handTransforms = child.GetComponentsInChildren<Transform>(true);
-
-                    foreach (Transform handChild in handTransforms)
-                    {
-                        string searchLabel = "txtMP_Element_";
-                        string goName = handChild.gameObject.name;
-
-                        if (goName.Contains(searchLabel))
-                        {
-                            string handNumberString = Regex.Match(goName, @"\d+").Value;
-                            int handNumber = int.Parse(handNumberString);
-
-                            TMPro.TextMeshProUGUI txt = handChild.gameObject.GetComponent<TMPro.TextMeshProUGUI>();
-
-                            txt.text = payoutTableModel.payouts[handNumber].coin1.ToString();
-
-
-
-                        }
-                    }
-                }
+            if( child.gameObject.name == HAND_COLUMN ||
+                child.gameObject.name == COIN_1_COLUMN ||
+                child.gameObject.name == COIN_2_COLUMN ||
+                child.gameObject.name == COIN_3_COLUMN ||
+                child.gameObject.name == COIN_4_COLUMN ||
+                child.gameObject.name == COIN_5_COLUMN )
+            {
+                InitColumn(child.gameObject.name, child.GetComponentsInChildren<Transform>(true));
             }
+        }
     }
 
-    private void InitColumn(string columnName, Transform child)
+    private void InitColumn(string columnName, Transform[] transforms)
     {
-        if (child.gameObject.name == columnName)
+        foreach (Transform txtChild in transforms)
         {
-            Transform[] handTransforms = child.GetComponentsInChildren<Transform>(true);
+            string searchLabel = "txtMP_Element_";
+            string goName = txtChild.gameObject.name;
 
-            foreach (Transform txtChild in handTransforms)
+            if (goName.Contains(searchLabel))
             {
-                string searchLabel = "txtMP_Element_";
-                string goName = txtChild.gameObject.name;
+                string handNumberString = Regex.Match(goName, @"\d+").Value;
+                int handNumber = int.Parse(handNumberString);
 
-                if (goName.Contains(searchLabel))
-                {
-                    string handNumberString = Regex.Match(goName, @"\d+").Value;
-                    int handNumber = int.Parse(handNumberString);
+                TMPro.TextMeshProUGUI txt = txtChild.gameObject.GetComponent<TMPro.TextMeshProUGUI>();
 
-                    TMPro.TextMeshProUGUI txt = txtChild.gameObject.GetComponent<TMPro.TextMeshProUGUI>();
-
-                    txt.text = GetElementText(columnName, handNumber);//payoutTableModel.payouts[handNumber].handName;
-                }
+                txt.text = GetElementText(columnName, handNumber);
             }
         }
     }
 
     private string GetElementText(string columnName, int handNumber)
     {
-        switch(columnName)
+        switch (columnName)
         {
-            case "Hand":
+            case HAND_COLUMN:
                 return payoutTableModel.payouts[handNumber].handName;
-            case "Coin1":
+            case COIN_1_COLUMN:
                 return payoutTableModel.payouts[handNumber].coin1.ToString();
-            case "Coin2":
+            case COIN_2_COLUMN:
                 return payoutTableModel.payouts[handNumber].coin2.ToString();
-            case "Coin3":
+            case COIN_3_COLUMN:
                 return payoutTableModel.payouts[handNumber].coin3.ToString();
-            case "Coin4":
+            case COIN_4_COLUMN:
                 return payoutTableModel.payouts[handNumber].coin4.ToString();
-            case "Coin5":
+            case COIN_5_COLUMN:
                 return payoutTableModel.payouts[handNumber].coin5.ToString();
         }
 
-        return "undefined";
+        return "undef";
     }
 
     // Update is called once per frame
