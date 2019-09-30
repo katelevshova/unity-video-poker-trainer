12d3ab496465094353c6f21121d6f2dd958af80f
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2019-09-30 10:01:12 -0700

    Issue#7 - added GameAssembly, Tests assembly, modified assembly file to add TextMeshPro

---
+++ VideoPokerJacksOrBetter/Assets/VideoPokerJacksOrBetter/Tests/HandAnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HandAnalyzerTest
+    {
+        [Test]
+        public void IsJacksOrBetter_Test()
+        {
+            //Handana
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator HandAnalyzerTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
